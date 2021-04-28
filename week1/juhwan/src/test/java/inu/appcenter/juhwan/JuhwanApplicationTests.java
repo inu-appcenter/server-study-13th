2a79551761738ecf2305a299606aea9043ff689b
juhwan0815 <juwom0831@naver.com>
2021-04-28 21:36:18 +0900

    test

---
+++ week1/juhwan/src/test/java/inu/appcenter/juhwan/JuhwanApplicationTests.java
@@ -0,0 +1,13 @@
+package inu.appcenter.juhwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuhwanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
